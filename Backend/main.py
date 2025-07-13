adbc24d112bbd5354bb2c444e94f0f87b61fb4cf
niduniDK <nidunikasige@gmail.com>
2025-07-13 10:14:53 +0530

    Update

--- Backend/main.py
+++ Backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import request_router
+import services.DSA_helper
 
 app = FastAPI()
 
@@ -9,10 +10,12 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=['http://localhost:5173'],
     allow_credentials=True,
-    allow_methods=['*']
+    allow_methods=['*'],
+    allow_headers=['*']
 )
 
 app.include_router(request_router.router, prefix='/chat', tags=['Chatbot'])
+app.include_router(services.DSA_helper.router, prefix='/guide', tags=['DSA Helper'])
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000, reload=True)
